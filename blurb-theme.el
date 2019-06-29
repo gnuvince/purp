f821a7c30452d970ccb0ee08b68d56603860e31d
Vincent Foley <vfoley@gmail.com>
2019-06-29 14:29:46 -0400

    Fix blurb

--- blurb-theme.el
+++ blurb-theme.el
@@ -1,17 +1,14 @@
-;;; blurb --- an Emacs theme
+;;; purp-light-theme.el --- A light color theme with few colors
+
+;;; Copyright (C) 2018 Vincent Foley
+
+;; Author: Vincent Foley <vfoley@gmail.com>
+;; URL: https://github.com/gnuvince/purp
+;; Version: 1.0
+;; Keywords: faces
 
 ;;; Commentary:
-
-;;; I find that most themes highlight too many things
-;;; and that makes nothing really stand out.  In blurb,
-;;; I try to highlight only a few faces:
-;;;
-;;; - string and character literals
-;;; - comments
-;;; - function declarations
-;;;
-;;; Everything else is kept to the default foreground color.
-
+;;; This is the light variant of purp.
 
 ;;; Code:
 
@@ -20,23 +17,29 @@
 (deftheme blurb
   "a theme that highlights only a few constructs")
 
-(mktheme 'blurb
-         :default-fg          "#fffbf6"
-         :string-fg           "#f6c744"
-         :function-fg         "#49a4f8"
-         :comment-fg          "#a47de9"
+(purp-common-mktheme
+ 'blurb
+ :default-fg          "#fffbf6"
+ :string-fg           "#f6c744"
+ :function-fg         "#49a4f8"
+ :comment-fg          "#a47de9"
 
-         :default-bg          "#101421"
-         :bright-bg           "#565656"
-         :inactive-bg         "#2e2e2e"
+ :default-bg          "#101421"
+ :bright-bg           "#565656"
+ :inactive-bg         "#2e2e2e"
 
-         :error-fg            "#ffff00"
-         :error-bg            "#aa0000"
+ :error-fg            "#ffff00"
+ :error-bg            "#aa0000"
 
-         ;; special cases
-         :hl-line-bg          "#303030"
-         )
+ ;; special cases
+ :hl-line-bg          "#303030"
+ )
+
+;;;###autoload
+(when load-file-name
+  (add-to-list 'custom-theme-load-path
+               (file-name-as-directory (file-name-directory load-file-name))))
 
 (provide-theme 'blurb)
 
-;;; blurb-theme ends here
+;;; blurb-theme.el ends here
